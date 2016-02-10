f4546fece367ae4932fc214feeb7b5cceb72d72f
AlejandroSilva <pm5k.sk@gmail.com>
2016-02-10 16:40:16 -0300

    Direcciones migration + modelo

---
+++ database/migrations/2016_02_10_151216_create_direcciones_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDireccionesTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(){
+        Schema::create('direcciones', function (Blueprint $table) {
+            // PK
+            $table->increments('idDireccion');
+
+            // FK
+            // cutComuna de la tabla Comunas
+            $table->integer('cutComuna');
+            $table->foreign('cutComuna')
+                ->references('cutComuna')
+                ->on('comunas');
+
+            // Otros campos
+            $table->string('direccion', 150);
+            $table->string('referencia', 150);
+            $table->string('gmapShortUrl', 40)->nullable();     // Ejemplo: https://goo.gl/maps/g4NBaThDwWQ2 (32 largo)
+            $table->text('gmapIframeUrl')->nullable();      // Ejemplo: https://www.google.com/maps/embed?pb=!1m14!1m8!.....+Metropolitana!5e0!3m2!...4v1455118804482 (282 largo)
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::drop('direcciones');
+    }
+}
