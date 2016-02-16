04e14916fb1bff540f9cbd8ddf9cc0f3feb75259
AlejandroSilva <pm5k.sk@gmail.com>
2016-02-16 09:12:31 -0300

    mockup de los menus

---
+++ resources/views/usuarios/usuarios.blade.php
@@ -0,0 +1,9 @@
+@extends('usuarios.layoutUsuarios')
+
+@section('title', 'Usuarios')
+
+@section('content')
+    <h1 class="page-header">Usuarios</h1>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit illum maxime mollitia, odio qui reprehenderit! Alias amet aspernatur atque dignissimos, eos id ipsam iusto, neque non similique, sint sunt totam?</p>
+@stop
