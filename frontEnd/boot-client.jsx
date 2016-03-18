d2a19bc0991bb18e30c77d65abb05814bc2f8de3
AlejandroSilva <pm5k.sk@gmail.com>
2016-03-18 11:32:47 -0300

    comienzo de programacion semanal

--- frontEnd/boot-client.jsx
+++ frontEnd/boot-client.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 
 // Componentes
 import InventarioForm from './components/inventario/InventarioForm.jsx'
-import ProgramacionMensual from './components/programacion/ProgramacionMensual.jsx'
+import ProgramacionMensual from 'components/programacionMensual/ProgramacionMensual.jsx'
 
 // Carga el componente dependiendo del elemento DOM en la pagina
 let nuevoInventarioDOM = document.getElementById('nuevo-inventario')
@@ -26,3 +26,21 @@ if( programacionMensualDOM ){
             clientes={window.laravelClientes}
         />, programacionMensualDOM)
 }
+
+// http://jamesknelson.com/push-state-vs-hash-based-routing-with-react-js/
+//class App extends React.Component{
+//    render(){
+//        console.log('render componente')
+//        return <div>
+//            <h1>golas {this.props.route}</h1>
+//        </div>
+//    }
+//}
+//
+//let hashChangeHandler = ()=>{
+//    let hashes = window.location.hash.replace(/^#\/?|\/$/g, '').split('/')
+//
+//    ReactDOM.render( <App route={hashes[0]}/>, document.getElementById('rc-top-menu'))
+//}
+//hashChangeHandler()
+//window.addEventListener('hashchange', hashChangeHandler, false)
