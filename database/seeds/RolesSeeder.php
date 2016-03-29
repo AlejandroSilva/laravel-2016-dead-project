0194f2edd3efca124db57cb4daad88509151af47
AlejandroSilva <pm5k.sk@gmail.com>
2016-03-29 09:24:25 -0300

    agregado Rol Supervisor al seeder

--- database/seeds/RolesSeeder.php
+++ database/seeds/RolesSeeder.php
@@ -9,6 +9,7 @@ class RolesSeeder extends Seeder {
             ['id'=> 2, 'name'=>'Lider',            'display_name'=>'Lider',        'description'=>'Lider de los inventarios'],
             ['id'=> 3, 'name'=>'Captador',         'display_name'=>'Captador',     'description'=>'Capta operadores para los inventarios'],
             ['id'=> 4, 'name'=>'Operador',         'display_name'=>'Operador',     'description'=>'Opera los capturadores en los inventarios'],
+            ['id'=> 5, 'name'=>'Supervisor',       'display_name'=>'Supervisor',   'description'=>'Supervisa a los operadores en los inventarios'],
         ]);
     }
 }
