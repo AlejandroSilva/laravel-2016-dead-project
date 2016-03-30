f0e0699f5ebd1979bce12a67ec2f9379b277bba1
AlejandroSilva <pm5k.sk@gmail.com>
2016-03-30 18:47:20 -0300

    fix css

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -29,7 +29,7 @@ module.exports = {
         ]
     },
     plugins: [
-        //new webpack.optimize.UglifyJsPlugin(),
+        new webpack.optimize.UglifyJsPlugin(),
         new ExtractTextPlugin('css/frontEnd.css', { allChunks: true })
         //new webpack.DefinePlugin({
         //    APP_NAME: `'${appConfig.name}'`,
