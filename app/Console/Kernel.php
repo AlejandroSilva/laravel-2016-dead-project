2e03a3e9733d481ba75d486f57c79d8db914e360
AlejandroSilva <pm5k.sk@gmail.com>
2016-05-06 08:57:34 -0300

    se deshabilita el CRON: genera un archivo de logs con permisos en asilva, lo que da problemas en las rutas

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,6 @@ class Kernel extends ConsoleKernel {
     protected function schedule(Schedule $schedule) {
         //$schedule->command('inspire')->hourly();
         
-        $schedule->command('nominas:pendientes')->dailyAt('08:00');	;
+        //$schedule->command('nominas:pendientes')->dailyAt('08:00');	;
     }
 }
