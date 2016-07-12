24f8e136e6b0d477e1766b9839e103c792561fe2
AlejandroSilva <pm5k.sk@gmail.com>
2016-07-12 09:24:51 -0400

    orden al archivo routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,9 +24,9 @@ Route::group(['middleware' => ['web']], function (){
     |*/
     Route::group(['middleware'=>['auth']], function(){
         // MANTENEDOR CLIENTES Y LOCALES
-        Route::get('admin/clientes',        'ClientesController@show_Lista')->name('admin.clientes.lista');
-        Route::get('admin/locales',         'LocalesController@show_mantenedor')->name('admin.locales.lista');
-        Route::get('admin/stock',           'StockController@show_mantenedorStock');
+        Route::get('admin/clientes',                                'ClientesController@show_Lista')->name('admin.clientes.lista');
+        Route::get('admin/locales',                                 'LocalesController@show_mantenedor')->name('admin.locales.lista');
+        Route::get('admin/stock',                                   'StockController@show_mantenedorStock');
         // USUARIOS -MANTENEDOR USUARIOS-ROLES
         Route::get('admin/usuarios-roles',                          'AuthController@show_usuarios_roles');
         // PERMISSIONS- MANTENEDOR PERMISSIONS-ROLES
@@ -39,13 +39,13 @@ Route::group(['middleware' => ['web']], function (){
         Route::get('user/changePassword',                           'AuthController@show_changePassword');
         Route::post('user/changePassword',                          'AuthController@post_change_password');
         // INVENTARIOS - MANTENEDOR (DESARROLLO DETENIDO)
-//        Route::get('inventario',                  'InventariosController@showIndex');
-//        Route::get('inventario/nuevo',            'InventariosController@showNuevo');
-//        Route::get('inventario/lista',            'InventariosController@showLista');
+//        Route::get('inventario',                                  'InventariosController@showIndex');
+//        Route::get('inventario/nuevo',                            'InventariosController@showNuevo');
+//        Route::get('inventario/lista',                            'InventariosController@showLista');
         // INVENTARIOS - PROGRAMACION IG
-        Route::get('programacionIG',                  'InventariosController@showProgramacionIndex');
-        Route::get('programacionIG/mensual',          'InventariosController@showProgramacionMensual');
-        Route::get('programacionIG/semanal',          'InventariosController@showProgramacionSemanal');
+        Route::get('programacionIG',                                'InventariosController@showProgramacionIndex');
+        Route::get('programacionIG/mensual',                        'InventariosController@showProgramacionMensual');
+        Route::get('programacionIG/semanal',                        'InventariosController@showProgramacionSemanal');
         // INVENTARIOS - DESCARGA DE PDF
         Route::get('/pdf/inventarios/{mes}/cliente/{idCliente}',    'InventariosController@descargarPDF_porMes');
         Route::get('/pdf/inventarios/{fechaInicial}/al/{fechaFinal}/cliente/{idCliente}',   'InventariosController@descargarPDF_porRango');
@@ -55,23 +55,23 @@ Route::group(['middleware' => ['web']], function (){
         Route::get('nominas/captadores',                            'NominasController@show_captadores');
         //Route::get('nominas/captador/{idCaptador}',                 'NominasController@show_nominasCaptador');
         // AUDITORIAS - PROGRAMACION AI
-        Route::get('programacionAI',                 'AuditoriasController@showProgramacionIndex');
-        Route::get('programacionAI/mensual',          'AuditoriasController@showMensual');
-        Route::get('programacionAI/semanal',          'AuditoriasController@showSemanal');
+        Route::get('programacionAI',                                'AuditoriasController@showProgramacionIndex');
+        Route::get('programacionAI/mensual',                        'AuditoriasController@showMensual');
+        Route::get('programacionAI/semanal',                        'AuditoriasController@showSemanal');
         // AUDITORIAS - DESCARGA DE PDF
         Route::get('pdf/auditorias/{mes}/cliente/{idCliente}',     'AuditoriasController@descargarPDF_porMes');
         Route::get('pdf/auditorias/{fechaInicial}/al/{fechaFinal}/cliente/{idCliente}',     'AuditoriasController@descargarPDF_porRango');
 
         // ACTIVOS FIJOS
-        Route::get('activo-fijo',             'ActivosFijosController@get_index');
+        Route::get('activo-fijo',                                   'ActivosFijosController@get_index');
         // api (ordenar luego)
 
-        Route::get('api/activo-fijo/cargar-productos',                      'MaestraController@api_cargar_productos');  // ELIMINAR
-        Route::get('api/activo-fijo/cargar-articulos',                      'MaestraController@api_cargar_articulos');  // ELIMINAR
+        Route::get('api/activo-fijo/cargar-productos',              'MaestraController@api_cargar_productos');  // ELIMINAR
+        Route::get('api/activo-fijo/cargar-articulos',              'MaestraController@api_cargar_articulos');  // ELIMINAR
 
         
         // GEO - MANTENEDOR (DESARROLLO DETENIDO)
-//        Route::get('geo',             'GeoController@show_index');//->name('geo.index');
+//        Route::get('geo',                                         'GeoController@show_index');//->name('geo.index');
     });
     /*
     |--------------------------------------------------------------------------
@@ -91,16 +91,16 @@ Route::group(['middleware' => ['web']], function (){
     |*/
     Route::group(['prefix'=>'api',  'middleware'=>['authAPI']], function() {
         // API CLIENTES Y LOCALES
-        Route::get('clientes',                      'ClientesController@api_getClientes');
-        Route::get('cliente/{idCliente}/locales',   'LocalesController@api_getLocales');
-        Route::post('locales',                      'LocalesController@api_nuevo');
-        Route::put('local/{idLocal}',             'LocalesController@api_actualizar');
-//        Route::get('locales/{idLocal}/verbose',     'LocalesController@api_getLocalVerbose');
+        Route::get('clientes',                                      'ClientesController@api_getClientes');
+        Route::get('cliente/{idCliente}/locales',                   'LocalesController@api_getLocales');
+        Route::post('locales',                                      'LocalesController@api_nuevo');
+        Route::put('local/{idLocal}',                               'LocalesController@api_actualizar');
+//        Route::get('locales/{idLocal}/verbose',                   'LocalesController@api_getLocalVerbose');
         // API INVENTARIOS
-        Route::post('inventario/nuevo',                 'InventariosController@api_nuevo');
-        Route::get('inventario/{idInventario}',         'InventariosController@api_get');
-        Route::put('inventario/{idInventario}',         'InventariosController@api_actualizar');
-        Route::delete('inventario/{idInventario}',      'InventariosController@api_eliminar');
+        Route::post('inventario/nuevo',                             'InventariosController@api_nuevo');
+        Route::get('inventario/{idInventario}',                     'InventariosController@api_get');
+        Route::put('inventario/{idInventario}',                     'InventariosController@api_actualizar');
+        Route::delete('inventario/{idInventario}',                  'InventariosController@api_eliminar');
         // API DE NOMINAS
         Route::put('nomina/{idNomina}',                             'NominasController@api_actualizar');
         Route::get('nominas/buscar',                                'NominasController@api_buscar');
@@ -121,30 +121,30 @@ Route::group(['middleware' => ['web']], function (){
         Route::post('nomina/{idNomina}/estado-rectificar',          'NominasController@api_rectificarNomina');
 
         // API AUDITORIAS
-        Route::post('auditoria/nuevo', 'AuditoriasController@api_nuevo');
-        Route::put('auditoria/{idAuditoria}', 'AuditoriasController@api_actualizar');
-        Route::delete('auditoria/{idAuditoria}', 'AuditoriasController@api_eliminar');
+        Route::post('auditoria/nuevo',                              'AuditoriasController@api_nuevo');
+        Route::put('auditoria/{idAuditoria}',                       'AuditoriasController@api_actualizar');
+        Route::delete('auditoria/{idAuditoria}',                    'AuditoriasController@api_eliminar');
         
         // API VISTA GENERAL
-        Route::get('vista-general/nominas-inventarios',    'VistaGeneralController@api_obtenerNominasAuditorias');
+        Route::get('vista-general/nominas-inventarios',             'VistaGeneralController@api_obtenerNominasAuditorias');
 
         // API ACTIVO FIJO
-        Route::get('activo-fijo/productos/buscar',              'ActivosFijosController@api_productos_buscar');
-        Route::get('activo-fijo/articulos/buscar',              'ActivosFijosController@api_articulos_buscar');
-        Route::get('activo-fijo/articulos/buscar-barra',        'ActivosFijosController@api_articulos_buscarBarra');
-        Route::post('activo-fijo/articulos/entregar',           'ActivosFijosController@api_articulos_entregar_a_almacen');
-        Route::post('activo-fijo/articulos/transferir',         'ActivosFijosController@api_articulos_transferir');
-        Route::get('activo-fijo/almacenes/buscar',              'ActivosFijosController@api_almacenes_buscar');
-        Route::post('activo-fijo/almacen/nuevo',                'ActivosFijosController@api_almacen_nuevo');
-        Route::get('activo-fijo/preguias/buscar',               'ActivosFijosController@api_preguias_buscar');
-        Route::get('activo-fijo/preguia/{idPreguia}',           'ActivosFijosController@api_preguia_fetch');
-        Route::post('activo-fijo/preguia/{idPreguia}/devolver', 'ActivosFijosController@api_preguia_devolver');
-        Route::get('activo-fijo/responsables/buscar',           'ActivosFijosController@api_responsables_buscar');
+        Route::get('activo-fijo/productos/buscar',                  'ActivosFijosController@api_productos_buscar');
+        Route::get('activo-fijo/articulos/buscar',                  'ActivosFijosController@api_articulos_buscar');
+        Route::get('activo-fijo/articulos/buscar-barra',            'ActivosFijosController@api_articulos_buscarBarra');
+        Route::post('activo-fijo/articulos/entregar',               'ActivosFijosController@api_articulos_entregar_a_almacen');
+        Route::post('activo-fijo/articulos/transferir',             'ActivosFijosController@api_articulos_transferir');
+        Route::get('activo-fijo/almacenes/buscar',                  'ActivosFijosController@api_almacenes_buscar');
+        Route::post('activo-fijo/almacen/nuevo',                    'ActivosFijosController@api_almacen_nuevo');
+        Route::get('activo-fijo/preguias/buscar',                   'ActivosFijosController@api_preguias_buscar');
+        Route::get('activo-fijo/preguia/{idPreguia}',               'ActivosFijosController@api_preguia_fetch');
+        Route::post('activo-fijo/preguia/{idPreguia}/devolver',     'ActivosFijosController@api_preguia_devolver');
+        Route::get('activo-fijo/responsables/buscar',               'ActivosFijosController@api_responsables_buscar');
 
         // API USUARIOS
-        Route::put('usuario/{idUsuario}',           'PersonalController@api_usuario_actualizar');
-        Route::get('usuarios/buscar',               'PersonalController@api_usuarios_buscar');
-        Route::post('usuarios/nuevo-operador',      'PersonalController@api_operador_nuevo');
+        Route::put('usuario/{idUsuario}',                           'PersonalController@api_usuario_actualizar');
+        Route::get('usuarios/buscar',                               'PersonalController@api_usuarios_buscar');
+        Route::post('usuarios/nuevo-operador',                      'PersonalController@api_operador_nuevo');
         // API USUARIOS-ROLES
         Route::post('usuario/{idUsuario}/role/{idRole}',            'AuthController@api_nuevo_rol');
         Route::delete('usuario/{idUsuario}/role/{idRole}',          'AuthController@api_delete_rol');
@@ -156,15 +156,15 @@ Route::group(['middleware' => ['web']], function (){
         Route::put('permission/{idPermission}/editar',              'AuthController@api_permission_actualizar');
         Route::delete('permission/{idPermission}',                  'AuthController@api_permission_eliminar');
         //API MANTENEDOR ROLES
-        Route::post('roles',                        'AuthController@api_role_nuevo');
-        Route::put('role/{idRole}',                 'AuthController@api_role_actualizar');
-        Route::delete('role/{idRole}',              'AuthController@api_eliminarRole');
+        Route::post('roles',                                        'AuthController@api_role_nuevo');
+        Route::put('role/{idRole}',                                 'AuthController@api_role_actualizar');
+        Route::delete('role/{idRole}',                              'AuthController@api_eliminarRole');
 
         // API GEO (DESARROLLO DETENIDO)
-//        Route::get('geo/comunas',      'GeoController@api_getComunas');
-        //  Route::get('stock/leerArchivo',             'StockController@api_leerArchivo');
-        Route::post('stock/upload',                 'StockController@api_uploadArchivo');
-        Route::post('stock/pegar',                  'StockController@api_pegarDatos');
+//        Route::get('geo/comunas',                                 'GeoController@api_getComunas');
+//        Route::get('stock/leerArchivo',                           'StockController@api_leerArchivo');
+        Route::post('stock/upload',                                 'StockController@api_uploadArchivo');
+        Route::post('stock/pegar',                                  'StockController@api_pegarDatos');
     });
     /*
     |--------------------------------------------------------------------------
@@ -173,20 +173,20 @@ Route::group(['middleware' => ['web']], function (){
     |*/
     Route::group(['prefix'=>'api'], function(){
         // API INVENTARIOS
-        Route::get('inventarios/buscar',                   'InventariosController@api_buscar');
-        Route::post('inventarios/informar-archivo-final',  'InventariosController@api_informarArchivoFinal');
+        Route::get('inventarios/buscar',                            'InventariosController@api_buscar');
+        Route::post('inventarios/informar-archivo-final',           'InventariosController@api_informarArchivoFinal');
         // API DE NOMINAS
         Route::post('nomina/cliente/{idCliente}/ceco/{CECO}/dia/{fecha}/informar-disponible', 'NominasController@api_informarDisponible');
         Route::post('nomina/cliente/{idCliente}/ceco/{CECO}/dia/{fecha}/informar-nomina-pago','NominasController@api_informarNominaPago');
         // API AUDITORIAS
-        Route::get('auditoria/mes/{annoMesDia}/cliente/{idCliente}',        'AuditoriasController@api_getPorMesYCliente');
-        Route::get('auditoria/{fecha1}/al/{fecha2}/cliente/{idCliente}',    'AuditoriasController@api_getPorRangoYCliente');
-        Route::get('auditoria/{fecha1}/al/{fecha2}/auditor/{idCliente}',  'AuditoriasController@api_getPorRangoYAuditor');
-        Route::get('auditoria/cliente/{idCliente}/dia/{annoMesDia}/estado-general',   'AuditoriasController@api_estadoGeneral');
-        Route::post('auditoria/cliente/{idCliente}/ceco/{CECO}/fecha/{fecha}/informar-realizado', 'AuditoriasController@api_informarRealizado');
-        Route::post('auditoria/cliente/{idCliente}/ceco/{CECO}/fecha/{fecha}/informar-fecha', 'AuditoriasController@api_informarFecha');
+        Route::get('auditoria/mes/{annoMesDia}/cliente/{idCliente}',            'AuditoriasController@api_getPorMesYCliente');
+        Route::get('auditoria/{fecha1}/al/{fecha2}/cliente/{idCliente}',        'AuditoriasController@api_getPorRangoYCliente');
+        Route::get('auditoria/{fecha1}/al/{fecha2}/auditor/{idCliente}',        'AuditoriasController@api_getPorRangoYAuditor');
+        Route::get('auditoria/cliente/{idCliente}/dia/{annoMesDia}/estado-general',                 'AuditoriasController@api_estadoGeneral');
+        Route::post('auditoria/cliente/{idCliente}/ceco/{CECO}/fecha/{fecha}/informar-realizado',   'AuditoriasController@api_informarRealizado');
+        Route::post('auditoria/cliente/{idCliente}/ceco/{CECO}/fecha/{fecha}/informar-fecha',       'AuditoriasController@api_informarFecha');
         // API USUARIOS
-        Route::get('usuario/{idUsuario}/roles', 'PersonalController@api_getRolesUsuario');
-        Route::get('usuarios/descargar-excel',  'PersonalController@excel_descargarTodos');
+        Route::get('usuario/{idUsuario}/roles',                     'PersonalController@api_getRolesUsuario');
+        Route::get('usuarios/descargar-excel',                      'PersonalController@excel_descargarTodos');
     });
 });
