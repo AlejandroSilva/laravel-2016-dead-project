10291b51ae431763a56226d72f5da9064f502b23
AlejandroSilva <pm5k.sk@gmail.com>
2016-09-08 16:43:05 -0300

    mas cambios

--- app/Http/Controllers/UtilConsolidadoController.php
+++ app/Http/Controllers/UtilConsolidadoController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests;
-// DB
-use DB;
 // PHPExcel
 use PHPExcel;
 use PHPExcel_IOFactory;
@@ -14,7 +12,9 @@ use PHPExcel_Shared_Date;
 class UtilConsolidadoController extends Controller {
 
     public function actualizarConsolidado(Request $request) {
-        DB::beginTransaction();
+        // antes de cargar el archivo, es buena idea exportar el xlsx a csv, abrir el csv y transformarlo nuevamente
+        // a xlsx. De esta forma se puede reducir hasta un 60% el tamaño del archivo, entonces la carga es mas rapida
+
         try {
             $nombreArchivo = $request->get('archivo');
             if(!isset($nombreArchivo))
@@ -24,34 +24,26 @@ class UtilConsolidadoController extends Controller {
             set_time_limit(0);
 
             $tableData = $this->leerConsolidado($nombreArchivo);
+            $query = '';
             foreach ($tableData as $data){
                 // si tiene una fecha nueva, actualizarla
                 if($data->expiracionNuevo!=null) {
-                   $query = "update SEI_INVENTARIO.archivo_auditoria_fecha set fecha_exp='FECHA NUEVA2'
-                             where barra = '$data->barra'
-                             and rut_auditor = '$data->rutAuditor'
-                             and rut_qf = '$data->rutQF'
-                             and tienda=$data->tienda
-                             and fecha_auditoria BETWEEN '2016-08-01' AND '2016-08-31'";
-                    DB::update(DB::raw($query));
+                    $query = $query." update SEI_INVENTARIO.archivo_auditoria_fecha set fecha_exp='$data->expiracionNuevo' ".
+                             "where barra = '$data->barra' ".
+                             "and tienda=$data->tienda ".
+                             "and fecha_auditoria BETWEEN '2016-08-01' AND '2016-08-31'; ";
                 }
                 if($data->loteNuevo!=null){
-                    $query = "update SEI_INVENTARIO.archivo_auditoria_fecha set lote='LOTE NUEVO2'
-                             where barra = '$data->barra'
-                             and rut_auditor = '$data->rutAuditor'
-                             and rut_qf = '$data->rutQF'
-                             and tienda=$data->tienda
-                             and fecha_auditoria BETWEEN '2016-08-01' AND '2016-08-31'";
-                    DB::update(DB::raw($query));
+                    $query = $query." update SEI_INVENTARIO.archivo_auditoria_fecha set lote='$data->loteNuevo' ".
+                             "where barra = '$data->barra' ".
+                             "and tienda=$data->tienda ".
+                             "and fecha_auditoria BETWEEN '2016-08-01' AND '2016-08-31'; ";
                 }
             }
-
-            DB::commit();
-            return response()->json('ok');
+            return response()->json($query);
         } catch (EXCEPTION $e) {
-            DB::rollback();
             throw $e;
-            return response()->json($e->message);
+            return response()->json('excepcion '.$e->message);
         }
     }
 
@@ -75,36 +67,10 @@ class UtilConsolidadoController extends Controller {
         // iniciar el 2, para saltarse el titulo
         for ($rowIndex = 2; $rowIndex <= $highestRow; $rowIndex++){
             $rowData = (object)[
-                // A 1
-                // B TIENDA
-                'tienda' => $sheet->getCell("B$rowIndex")->getValue(),
-                // C FECHA          ****
-                // D AUDITOR
-                // E RUT AUDITOR    ****
-                // F NOMBRE QF
-                // G RUT QF         ****
-                // 'fecha'=> $sheet->getCell("C$rowIndex")->getValue(),
-                'rutAuditor' => $sheet->getCell("E$rowIndex")->getValue(),
-                'rutQF'=> $sheet->getCell("G$rowIndex")->getValue(),
-                // H ITEMS
-                // I SKU
-                // J BARRA
+                'tienda' => $sheet->getCell("A$rowIndex")->getValue(),
                 'barra'=> $sheet->getCell("J$rowIndex")->getValue(),
-                // K DESCRIPTOR
-                // L LABORATORIO
-                // M LOTE           *****
-                // N NUEVO LOTE     *****
-                'loteAntiguo'=> $sheet->getCell("M$rowIndex")->getValue(),
                 'loteNuevo'=> $sheet->getCell("N$rowIndex")->getValue(),
-
-                // O FECHA EXP      *****
-                // P NUEVA FECHA    *****
-                'expiracionAntiguo'=> $sheet->getCell("O$rowIndex")->getValue(),
                 'expiracionNuevo'=> $sheet->getCell("P$rowIndex")->getValue(),
-                // Q IMAGEN
-                // R HORA
-                // S ??
-                // T RUT Julio
             ];
             // si la fila tiene una modificacion, entonces se agrega a la lista
             if($rowData->loteNuevo!=null || $rowData->expiracionNuevo!=null){
