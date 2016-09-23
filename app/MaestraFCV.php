94bad83dfc43c8f971ef176ddae7281f94a843ed
cpobleteolmedo <carlospobleteolmedo@gmail.com>
2016-09-23 16:39:17 -0300

    modelo maestra_fcv_table y archivo_maestra_fcv_table

---
+++ app/MaestraFCV.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MaestraFCV extends Model{
+    public $table = 'maestra_fcv';
+    // PK
+    public $primaryKey = 'idMaestraFCV';
+    public $timestamps = true;
+    // Campos asignables
+    protected $fillable = ['idArchivoMaestra',  'codigoProducto', 'descriptor', 'codigo', 'laboratorio', 'clasificacionTerapeutica'];
+
+    //Relaciones
+    public function archivoMaestraFCV(){
+        return $this->belongsTo('App\ArchivoMaestraFCV', 'idArchivoMaestra', 'idArchivoMaestra');
+    }
+}
