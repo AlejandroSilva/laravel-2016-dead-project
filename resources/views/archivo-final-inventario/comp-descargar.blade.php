5e2f1a7ae951285a92999e2dcc9fb38f7bfc01bc
AlejandroSilva <pm5k.sk@gmail.com>
2016-10-12 15:37:18 -0300

    buscar actas por rango de fechas, y ordenadas por inventario.fechaProgramada

--- resources/views/archivo-final-inventario/comp-descargar.blade.php
+++ resources/views/archivo-final-inventario/comp-descargar.blade.php
@@ -4,7 +4,6 @@
     </div>
     <div class="panel-body">
         <div class="col-sm-12">
-            <div style="height: 34px;"></div>
             <a class="btn btn-primary btn-sm btn-block" href="/inventario/descargar-consolidado-fcv?fechaInicio=2016-05-01">1 Mayo - Hoy</a>
             <a class="btn btn-primary btn-sm btn-block" href="/inventario/descargar-consolidado-fcv?fechaInicio=2016-09-01&fechaFin=2016-9-30">Consolidado Septiembre</a>
             <a class="btn btn-primary btn-sm btn-block" href="/inventario/descargar-consolidado-fcv?fechaInicio=2016-10-01&fechaFin=2016-10-31">Consolidado Octubre</a>
